38102262b8196730c37b369d2832569e1aa4f607
AviralAgarwal009 <aviralagarwal82@gmail.com>
2020-05-16 16:59:11 +0530

    home page completed

--- src/main/java/com/springboot/EventManager/entity/Participants.java
+++ src/main/java/com/springboot/EventManager/entity/Participants.java
@@ -8,13 +8,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
+
+
 
 @Entity
 @Table(name = "participants")
-@Getter
-@Setter
 public class Participants {
 	@Id
 	@Column(name = "reg_id")
@@ -25,7 +23,7 @@ public class Participants {
 	private String name;
 
 	@Column(name = "mobile_number")
-	private int contact;
+	private Long contact;
 
 	@Column(name = "email_id")
 	private String email;
@@ -37,10 +35,108 @@ public class Participants {
 	private String regType;
 
 	@Column(name = "tickets_qty")
-	private int ticketsQty;
+	private Integer ticketsQty;
 
 	@Column(name = "date_time")
 	private Date dateTime;
+	
+
+
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public Long getContact() {
+		return contact;
+	}
+
+
+
+	public void setContact(Long contact) {
+		this.contact = contact;
+	}
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+	public Blob getIdentity() {
+		return identity;
+	}
+
+
+
+	public void setIdentity(Blob identity) {
+		this.identity = identity;
+	}
+
+
+
+	public String getRegType() {
+		return regType;
+	}
+
+
+
+	public void setRegType(String regType) {
+		this.regType = regType;
+	}
+
+
+
+	public Integer getTicketsQty() {
+		return ticketsQty;
+	}
+
+
+
+	public void setTicketsQty(Integer ticketsQty) {
+		this.ticketsQty = ticketsQty;
+	}
+
+
+
+	public Date getDateTime() {
+		return dateTime;
+	}
+
+
+
+	public void setDateTime(Date dateTime) {
+		this.dateTime = dateTime;
+	}
+
+
 
 	@Override
 	public String toString() {
