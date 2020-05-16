a38e63cb9663b2c4cc0239181e153b4781d5922d
Sarthak Joshi <60484437+sarthaksj@users.noreply.github.com>
2020-05-16 17:00:09 +0530

    "New Controller Added"

--- src/main/java/com/springboot/EventManager/Controller/FormController.java
+++ src/main/java/com/springboot/EventManager/Controller/FormController.java
@@ -1,20 +1,33 @@
 package com.springboot.EventManager.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.springboot.EventManager.Service.DetailsService;
 import com.springboot.EventManager.entity.Participants;
 
 @Controller
 public class FormController {
-	
+
+	@Autowired
+	DetailsService detailsService;
+
 	@GetMapping("/home")
 	public String showHome(Model model) {
-		
-		Participants participants=new Participants();
-		model.addAttribute("participant",participants);
+
+		Participants theParticipants = new Participants();
+		model.addAttribute("participant", theParticipants);
 		return "home";
-}
+	}
+
+	@PostMapping("/saveParticipant")
+	public String saveParticipant(@ModelAttribute("participant") Participants theParticipants) {
+		detailsService.saveParticipants(theParticipants);
+		return "redirect:/  ";
+	}
 
 }
--- src/main/java/com/springboot/EventManager/WebConfig/WebSecurityConfig.java
+++ src/main/java/com/springboot/EventManager/WebConfig/WebSecurityConfig.java
@@ -1,6 +1,9 @@
 package com.springboot.EventManager.WebConfig;
 
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,8 +12,23 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	DataSource dataSource;
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth)
+	  throws Exception {
+	    auth.jdbcAuthentication()
+	      .dataSource(dataSource);
+	}
+	
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
+		http.authorizeRequests()
+		.antMatchers("/home").permitAll()
+		.antMatchers("").hasRole(null);
+		
 	}
 }
