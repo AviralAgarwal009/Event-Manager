0de088c2cc774beed78601784fcacec7420cd488
Sarthak Joshi <60484437+sarthaksj@users.noreply.github.com>
2020-05-18 13:06:34 +0530

    local

--- EventsMangement.sql
+++ EventsMangement.sql
@@ -1,9 +1,9 @@
                                                    -- Uncomment These Lines for creating user
                                                    -- if you are running this script for the first time 
                                                    
-CREATE USER if not exists 'event_data'@'localhost' IDENTIFIED BY 'event_data';
+/*CREATE USER if not exists 'event_data'@'localhost' IDENTIFIED BY 'event_data';
 GRANT ALL PRIVILEGES ON * . * TO 'event_data'@'localhost';
-ALTER USER 'event_data'@'localhost' IDENTIFIED WITH mysql_native_password BY 'data_info_event';
+ALTER USER 'event_data'@'localhost' IDENTIFIED WITH mysql_native_password BY 'data_info_event';*/
 
 DROP DATABASE IF EXISTS `event_info`;
 CREATE DATABASE `event_info`;
@@ -13,9 +13,9 @@ USE `event_info`;
  CREATE TABLE `participants` (
   `reg_id` int(12) NOT NULL AUTO_INCREMENT,
   `full_name` varchar(60) NOT NULL,
-  `mobile_number`int(10) NOT NULL,
+  `mobile_number`long  NOT NULL,
   `email_id`varchar(60) NOT NULL,
-  `id_card`blob NOT NULL,
+  `id_card`blob(10000000) NOT NULL,
   `reg_type`varchar(10) NOT NULL,
   `tickets_qty`int(3) NOT NULL,
   `date_time`datetime NOT NULL,
@@ -36,10 +36,10 @@ CREATE TABLE `users` (
 
 
 -- Dumping data for table `users`
--- Default passwords here are: fun123
+-- Default password is : admin
 INSERT INTO `users` 
 VALUES 
-('admin','{bcrypt}$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K',1);
+('admin','{bcrypt}$2a$10$XsvZ5lOrIuQ0n4mc3NZ2v.nB6U66bczDjm4p9iOC5rKM57oTxdOSC',1);
 
 -- Table structure for table `authorities`
 
@@ -57,4 +57,5 @@ INSERT INTO `authorities`
 VALUES 
 ('admin','ROLE_ADMIN');
 
+SET GLOBAL max_allowed_packet = 1073741824;
 
